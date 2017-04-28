e9859397252c0f20ab941df7cce28578982a09b6
kethomas <kirsten.thomas@utexas.edu>
2017-04-28 14:14:50 +0200

    cleaning up code, changing documentation

--- main.cpp
+++ main.cpp
@@ -1,45 +1,13 @@
-//==============================================================================
 /*
-    Software License Agreement (BSD License)
-    Copyright (c) 2003-2016, CHAI3D.
-    (www.chai3d.org)
-
-    All rights reserved.
-
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions
-    are met:
-
-    * Redistributions of source code must retain the above copyright
-    notice, this list of conditions and the following disclaimer.
-
-    * Redistributions in binary form must reproduce the above
-    copyright notice, this list of conditions and the following
-    disclaimer in the documentation and/or other materials provided
-    with the distribution.
-
-    * Neither the name of CHAI3D nor the names of its contributors may
-    be used to endorse or promote products derived from this software
-    without specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-    POSSIBILITY OF SUCH DAMAGE. 
-
-    \author    <http://www.chai3d.org>
-    \author    Francois Conti
-    \version   3.2.0 $Rev: 1925 $
-*/
-//==============================================================================
+ *
+ *    DH2660 Haptic Programming Spring 2017
+ *    HapMap - Group 5 (Thea, Linnéa, Kirsten)
+ *    Code based on Chai3D Example 14
+ *
+ *    Distribution license: BSD (e.g. free to use for
+ *    most purposes, see end of file)
+ *
+ */
 
 //------------------------------------------------------------------------------
 #include "chai3d.h"
@@ -407,7 +375,7 @@ int main(int argc, char* argv[])
     // OBJECT 0: KTH Map - Thea, Linnéa, Kirsten
     ////////////////////////////////////////////////////////////////////////
 
-    // create a mesh
+    // create a multimesh
     object = new cMultiMesh();
 
     // create collision detector
@@ -435,16 +403,7 @@ int main(int argc, char* argv[])
         return (-1);
     }
 
-    // get dimensions of object
-    object->computeBoundaryBox(true);
-    double size = cSub(object->getBoundaryMax(), object->getBoundaryMin()).length();
-
-    // resize object to screen
-//    if (size > 0.001)
-//    {
-//        object->scale(1.0 / size);
-//    }
-
+    // set material of object
     cMaterial m;
     m.setBlueCadet();
     object->setMaterial(m);
@@ -682,3 +641,51 @@ void updateHaptics(void)
 }
 
 //------------------------------------------------------------------------------
+
+
+//==============================================================================
+/*
+ *
+ *  Example code borrowed from Chai3D library:
+
+    Software License Agreement (BSD License)
+    Copyright (c) 2003-2016, CHAI3D.
+    (www.chai3d.org)
+
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following
+    disclaimer in the documentation and/or other materials provided
+    with the distribution.
+
+    * Neither the name of CHAI3D nor the names of its contributors may
+    be used to endorse or promote products derived from this software
+    without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE. 
+
+    \author    <http://www.chai3d.org>
+    \author    Francois Conti
+    \version   3.2.0 $Rev: 1925 $
+*/
+//==============================================================================
+
